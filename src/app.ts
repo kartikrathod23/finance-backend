fc0f5f3a9f026ef0d9d6d00f47077a5f34ededa5
kartik rathod <rathodkartik293@gmail.com>
2026-04-02 23:42:32 +0530

    feat: add rate limiting middleware and fix middleware order

--- src/app.ts
+++ src/app.ts
@@ -5,16 +5,27 @@ import financeRoutes from "./modules/finance/finance.routes";
 import dashboardRoutes from "./modules/dashboard/dashboard.routes"
 import userRoutes from "./modules/user/user.routes";
 import { errorHandler } from "./middleware/error.middleware";
+import rateLimit from "express-rate-limit";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(errorHandler);
+
+//rate limiting
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,//15 minutes
+  max: 100, //limiting each IP to 100 requests
+  message: "Too many requests, please try again later",
+});
+
+app.use(limiter);
 
 app.use("/api/auth", authRoutes);
 app.use("/api/finance", financeRoutes);
 app.use("/api/dashboard", dashboardRoutes);
 app.use("/api/users",userRoutes);
+
+app.use(errorHandler);
 
 export default app;
